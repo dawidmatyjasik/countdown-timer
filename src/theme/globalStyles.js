aa3ee436511c3b694128b620b65cf55ab3287924
Dawid Spisak <dawidspisak@gmail.com>
2021-06-15 12:56:02 +0200

    setup styles

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -5,10 +5,13 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Spartan', sans-serif;
+    font-family: 'Red Hat Text', sans-serif;
 }
 body{
     width: 100vw;
     height: 100vh;
+}
+html{
+    font-size: 14px;
 }
 `;
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,1 +1,14 @@
-export const main = {};
+export const main = {
+  colors: {
+    primary: {
+      grayishBlue: "hsl(237, 18%, 59%)",
+      softRed: "hsl(345, 95%, 68%)",
+    },
+    neutral: {
+      white: "White: hsl(0, 0%, 100%)",
+      darkDesaturatedBlue: "hsl(236, 21%, 26%)",
+      veryDarkBlue: "hsl(235, 16%, 14%)",
+      mostlyBlack: "hsl(234, 17%, 12%)",
+    },
+  },
+};
